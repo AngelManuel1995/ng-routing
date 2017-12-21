b1ef1e0b41191718fa83d5a0f14ead8c6a42f3d4
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-12-21 13:35:30 -0500

    Example of ngSwith done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }         from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { NgStyleComponen } from './components/ng-style/ng-style.component';
-import { CssComponent }    from './components/css/css.component';
-import { ClassesComponent } from './components/classes/classes.component';
-import { HighlightedDirective }   from './directives/highlighted.directive';
+import { AppComponent }            from './app.component';
+import { NgStyleComponen }         from './components/ng-style/ng-style.component';
+import { CssComponent }            from './components/css/css.component';
+import { ClassesComponent }        from './components/classes/classes.component';
+import { HighlightedDirective }    from './directives/highlighted.directive';
+import { NgswitchComponent }       from './components/ng-swith/ng-switch.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HighlightedDirective }   from './directives/highlighted.directive';
     NgStyleComponen,
     CssComponent,
     ClassesComponent,
-    HighlightedDirective    
+    HighlightedDirective,
+    NgswitchComponent    
   ],
   imports: [
     BrowserModule,
