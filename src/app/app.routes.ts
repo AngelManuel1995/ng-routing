3ddcf931744f1b27270e02a894ccf2277e9b7701
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-12-21 22:37:47 -0500

    Parents and children routes inside the diferents file ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent }        from './components/home/home.component';
-import { UserComponent }				from './components/user/user.component';
-import { NewUserComponent }  from './components/user/new-user.component';
-import { EditUserComponent } from './components/user/edit-user.component';
-import { DetailUserComponent } from './components/user/detail-user.component';
+import { UserComponent }			  from './components/user/user.component';	
+import { USER_ROUTES }					from './components/user/user.routes' 	
 
 const APP_ROUTES:Routes = [
 	{ 
@@ -13,25 +11,7 @@ const APP_ROUTES:Routes = [
 	{
 		path:'user/:id',
 		component:UserComponent,
-		children:[
-			{
-				path:'new',
-				component:NewUserComponent
-			},
-			{
-				path:'edit',
-				component:EditUserComponent
-			},
-			{
-				path:'detail',
-				component:DetailUserComponent
-			},
-			{
-				path:'**', 
-				pathMatch:'full', 
-				redirectTo:'new'
-			}
-		]
+		children:USER_ROUTES
 	},
 	{
 		path:'**', 
